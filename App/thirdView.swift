03915383f89f4faabe8928ea0ce16919d0787815
赵希帆 <zhaoxifan1@zhaoxifandeMacBook-Pro.local>
2015-11-20 09:55:22 +0800

    ceshi

--- App/thirdView.swift
+++ App/thirdView.swift
@@ -11,6 +11,7 @@ import UIKit
 class thirdView : UIViewController {
     
     @IBAction func exitbtn(sender: AnyObject) {
+        //sss
          test = nil
         test0 = nil
         test2 = nil
